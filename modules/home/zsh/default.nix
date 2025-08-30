3f20fb00aa845aaaf7b7c1eeb0065bfb7ae9ea79
Domirando <vohidjonovnamaftuna@gmail.com>
2025-08-30 17:30:54 +0500

    feat: rebuild and git status aliases added

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -7,5 +7,9 @@
     initContent = ''
       eval "$(starship init zsh)"
     '';
+    shellAliases = {
+        nrs = "sudo nixos-rebuild switch --flake .";
+        gstatuses = "ls | xargs -i sh -c 'echo _______ {} ______ && cd {} && git status -s -uno && cd .. '";
+    };
   };
 }
