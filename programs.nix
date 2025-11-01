4c23d73443e080628540e24d10bfc5131cba4dbb
Domirando <vohidjonovnamaftuna@gmail.com>
2025-11-01 13:04:07 +0500

    test: vim fugitive plugin

--- programs.nix
+++ programs.nix
@@ -3,15 +3,28 @@
   pkgs,
   ...
 }: {
+  programs.zsh.enable = true;
   programs.gnupg.agent = {
     enable = true;
     pinentryPackage = with pkgs; pinentry-all;
     enableSSHSupport = true;
   };
+
   programs.direnv = {
     enable = true;
     loadInNixShell = false;
     nix-direnv.enable = true;
   };
-  programs.zsh.enable = true;
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+    #package = pkgs.vim-full;
+    package = pkgs.vim.customize {
+                                  name = "vim";
+                                 vimrcConfig.packages.myVim = with pkgs.vimPlugins; {
+                                    start = [fugitive];
+                                 opt = [elm-vim];
+                                 };
+                                };
+  };
 }
